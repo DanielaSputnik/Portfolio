30296aa4cb5db9be8e11fc2f3c20ab6693d377b5
Daniela Sputnik <you@example.com>
2021-12-04 19:00:21 +0100

    Add CV and modify About section

--- src/Components/Sections/Data.js
+++ src/Components/Sections/Data.js
@@ -5,7 +5,7 @@ export const homeObjOne = {
     lightTextDesc: true,
     topLine: 'Jr Front End Developer in making',
     headline: 'Daniela Drdakova',
-    description: 'I am an aspiring developer of front end applications with passion for coding, details and the Lord of the Rings (among many other things). Get my CV from the link below or continue scrolling to see more..',
+    description: 'I am an aspiring developer of front end applications with passion for coding, design, details and the Lord of the Rings (amongst many other things). Get my CV from the link below or continue scrolling to see more..',
     buttonLabel: 'Open CV',
     imgStart: true,
     img: require('../../Images/svg-1.svg').default,
@@ -15,21 +15,4 @@ export const homeObjOne = {
     darkText: true,
 }
 
-
-export const homeObjFour = {
-    id: 'experiences',
-    lightBg: false,
-    lightText: true,
-    lightTextDesc: false,
-    topLine: 'My rocky road (to Dublin?)',
-    headline: 'Experiences',
-    description: '',
-    buttonLabel: 'Tell Me More',
-    imgStart: false,
-    img: require('../../Images/svg-3.svg').default,
-    alt: 'AboutMe', 
-    dark: false,
-    primary: false,
-    darkText: false,
-}
 
